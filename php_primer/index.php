d990fa1ea41dc4220181d38673da193624514b74
betulsimsek <eylulbetulsimsek@gmail.com>
2023-06-28 20:29:22 +0300

    how PHP works

---
+++ php_primer/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - Index </title>
+</head>
+<body>
+    <h1>Hello </h1>
+    <br/>
+    <?php
+        echo "Hello PHP";
+        echo "<br/>";
+        echo "Second Line";
+        echo "<br/>";
+    ?>
+    <?php
+        $name = "Betul Simsek";
+        $age = 27;
+        echo $name;
+
+        echo "<h1> My name is: ".$name." </h1>"
+        echo "<h1> My age is: ".$age." </h1>"
+
+    ?>
+</body>
+</html>
